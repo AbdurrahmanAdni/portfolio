c3b3417813ba81f3826b8ef4a635f38673cb3a69
AbdAdni <13517117@std.stei.itb.ac.id>
2021-06-27 22:41:35 +0700

    header-1 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from "../src/components/Navbar.js"
+import Header from "../src/components/Header.js"
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      Portofolio
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/Header.css"
+
+function Header() {
+    return (
+        <div className = "header-wrapper">
+            <div className = "layer">
+                <div className = "main-info">
+                    <div className = "info">
+                        <h1>Web Development</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
